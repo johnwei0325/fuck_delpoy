41f5362cb9d191c1220dfd7d019b802beef5b89a
johnwei0325 <113231911+johnwei0325@users.noreply.github.com>
2022-12-27 19:54:38 +0800

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,10 +15,16 @@ import mongoose from 'mongoose'
 import { dataInit } from './upload'
 require('dotenv').config()
 const app = express()
+const corsOptions = {
+    optionsSuccessStatus: 200, // For legacy browser support
+    credentials: true, // This is important.
+    origin: "https://chalkcoin.io",
+};
+
 if (process.env.NODE_ENV === "development") {
-	
+	app.use(cors(corsOptions));
 }
-app.use(cors());
+
 if (process.env.NODE_ENV === "production") {
     const __dirname = path.resolve();
     app.use(express.static(path.join(__dirname, "../frontend", "build")));
@@ -30,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:4000/api/')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Credentials', 'true')
