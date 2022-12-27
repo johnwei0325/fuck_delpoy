ca9d76a99e5e5e2c98fe504b0ff36f5bd059a1c1
johnwei0325 <113231911+johnwei0325@users.noreply.github.com>
2022-12-27 19:44:52 +0800

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4000')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
     res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
     res.header('Access-Control-Allow-Credentials', 'true')
